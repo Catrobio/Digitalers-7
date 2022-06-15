e3ef8085328c809eb08b5921d6b44701e659ce5c
FRANCISCO IGNACIO <Catrobio@users.noreply.github.com>
2022-06-15 12:29:23 -0300

    Clase del 15 con agregado de modal y cambio agregado de clase y style con jquery

--- js/site.js
+++ js/site.js
@@ -118,8 +118,97 @@ const itensListaFlecha = (nombre) => '<li>' + nombre +'</li>';
 
 const nombreUsuario = (nombre,tipo) => nombre + " es: "+tipo;
 
-document.getElementById("usuario").innerHTML = nombreUsuario("Ignacio","Administrador");
 
+/*JQuery*/
+function agregarNombre(nombre){    
+    $("#usuario").html(nombre);
+}
+
+function alertNombre(){
+    let nombreDiv = $("#usuario").html();
+    alert(nombreDiv);
+}
+
+$("#enviar").click(
+    function(){
+        let nombre = $("#nombre").val();
+        let apellido = $("#apellido").val();
+        if(nombre != "" && apellido != "")
+        {
+            $("#usuario").html("Nombre: "+ nombre+" Apellido: "+apellido);
+        } 
+});
+
+
+function ajaxCallJquery(){
+    $.ajax({
+        url : "https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=7f8bed04850e4770e28039b7fe4e424c",
+        type: "GET",
+        success: function(data){
+            let result = JSON.stringify(data);
+            console.log(result);
+        },
+        error : function(err){
+            console.log("Error: "+ error);
+        }
+    })
+}
+
+function ajaxCallJs(){
+    let xhr = new XMLHttpRequest();
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=7f8bed04850e4770e28039b7fe4e424c";
+
+    xhr.open("GET",url,true);
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+        }
+        else{
+            console.log("Error: "+this.status);
+        }
+    }
+    xhr.send();
+}
+
+ajaxCallJs();
+//ajaxCall();
+
+//crear Modal
+//Inicializo el modal
+dialog = $("#dialog-form" ).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+    //Aca se argega el boton en el pie del modal. Para este ejemplo no lo utlizaremos pero igual lo estoy mostrando. (Le agregué un jquery para argegar style)
+    //El "addUser" es la funcion que ejecutá cuando le haga click y al cancelar ce cerrará el modal
+      "Crear Usurio": addUser,
+      Cancel: function() {
+        dialog.dialog( "close" );
+      }
+    }
+  });
+
+//Boton que abre el modal
+$("#crear").button().on( "click", function() {
+    dialog.dialog( "open" );
+  });
+
+//Ejemplo de agregar estilo con Jquery
+function addUser(){
+    alert("No tienen funcion este boton");  
+    $('#dialog-form').css('background', 'blue');  
+    //con esta codigo $("#dialog-form").addClass("rojo"); se puede agregar una clase de css previamente hecha en lugar de un estilo
+}
+//* y css
+//$("#dialog-form").css("display:none");
+//agregarNombre("Ignacio");
+//alertNombre();
+
+
+//document.getElementById("usuario").innerHTML = nombreUsuario("Ignacio","Administrador");
 //document.getElementById("ulejemplo").innerHTML = itensLista("Francisco");
  //alert(operacion(10,0,"/"));
 // retorno(x);
